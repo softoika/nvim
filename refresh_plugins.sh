da01c2a234a9bfa6e0af9ebf34105aff1bfb1e33
r-hanafusa <r_hanafusa@dreamarts.co.jp>
2019-10-01 09:15:50 +0900

    Move coc.nvim settings to lazy.toml

--- refresh_plugins.sh
+++ refresh_plugins.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 rm -rf ~/.local/share/dein/
-nvim after_refresh \
-  +CocInstall\ coc-tsserver \
-  +CocInstall\ coc-angular \
-  +CocInstall\ coc-json
+# nvim after_refresh \
+#   +CocInstall\ coc-tsserver \
+#   +CocInstall\ coc-angular \
+#   +CocInstall\ coc-prettier \
+#   +CocInstall\ coc-json
+nvim after_refresh
 
