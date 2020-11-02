4d579a1276c32df6d573e32a201ae02b9d5d0db9
softoika <zwgntz@gmail.com>
2020-11-02 21:26:32 +0900

    Make nvim-treesitter lazy loading

--- dein/treesitter.lua
+++ dein/treesitter.lua
@@ -2,7 +2,7 @@ require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true,              -- false will disable the whole extension
-    disable = { "teal", "python", "dart", "ocaml", "php", "fennel", "query", "verilog", "ql", "rst", "ocaml_interface" },
+    disable = {},
   },
 }
 
